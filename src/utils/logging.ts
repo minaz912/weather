0ffdec2b4672fb54c75b82eff547800df6dc0069
Mina S. Aziz <minaz922@gmail.com>
2019-09-30 00:01:29 +0200

    Log local time for each city

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,9 +1,13 @@
 export function showError(city: string, err?: Error): void {
   console.error(
     `City/ZipCode: ${city}, Error encountered: ${err || 'Unknown Error'}`
-  );
+  )
 }
 
-export function showResult(city: string, temperature: string): void {
-  console.log(`City/ZipCode: ${city}, Temperature: ${temperature}`);
+export function showResult(
+  city: string,
+  time: string,
+  temperature: string
+): void {
+  console.log(`City/ZipCode: ${city}, Time: ${time}, Weather: ${temperature}`)
 }
