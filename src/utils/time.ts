f199bb5b8a23bd7526fe76e1d612ea6dec1d807d
Mina S. Aziz <minaz922@gmail.com>
2019-09-29 23:47:39 +0200

    Handle missing time input

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,9 +1,9 @@
 import moment from 'moment-timezone';
 
-export default function getPrettyDateWithTzOffset(
-  utcOffsetInSeconds: number
-): string {
-  return moment()
-    .utcOffset(utcOffsetInSeconds / 60)
-    .format('LLL');
+export function getPrettyDateWithTzOffset(utcOffsetInSeconds?: number): string {
+  return utcOffsetInSeconds
+    ? moment()
+        .utcOffset(utcOffsetInSeconds / 60)
+        .format('LLL')
+    : 'N/A';
 }
