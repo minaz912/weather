a6bd483990a481624daa29671027c900c6b65eeb
Mina S. Aziz <minaz922@gmail.com>
2019-09-29 23:39:09 +0200

    Remove global usage and add FIXME

--- src/utils/args.ts
+++ src/utils/args.ts
@@ -1,11 +1,12 @@
 import { CategorizedArg } from '../types';
 
 export function categorizeCityArg(arg: string): CategorizedArg {
-  return arg.length >= 5 && arg.length <= 6 && !isNaN(Number(arg))
+  return arg.length >= 5 && arg.length <= 6 && !Number.isNaN(Number(arg))
     ? { queryType: 'ZIP_CODE', queryValue: arg }
     : { queryType: 'CITY_NAME', queryValue: arg };
 }
 
+// FIXME: handle city names that include spaces
 export function parseCityArg(arg: string): string {
   // if the arg ends in ',', discard the ','
   return arg[arg.length - 1] === ',' ? arg.slice(0, arg.length - 1) : arg;
